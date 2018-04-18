10c7d9390510e594092e24c348ee2b2c7d299cb7
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-04-18 09:37:19 -0700

    Calendars appears at sign up.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,118 +11,43 @@ import Button from 'react-bootstrap/lib/Button';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import Calendar from './components/Calendar.jsx';
+import Signin from './components/Signin.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
-      calendarRedirect: false
-      // remember: false // for adding more functionality later
+      view: ''
     };
   }
 
-  componentDidMount() {
-  }
-
-  handleUsernameChange(e) {
-    this.setState({ username: e.target.value });
-    //console.log(this.state.username);
-  }
-
-  handlePasswordChange(e) {
-    this.setState({ password: e.target.value });
-    //console.log(this.state.username);
-  }
-
-  onButtonClick(e) {
-    e.preventDefault();
-    var data = {username: this.state.username, password: this.state.password};
-    // need to change this to interact with server on click
-    $.ajax({
-      method: 'POST',
-      url: '/signin',
-      contentType: 'application/json',
-      data: JSON.stringify(data),
-      dataType: 'json',
-      success: (data) => {
-        console.log('Success! Should redirect to calendar');
-        this.setState({
-          calendarRedirect: true
-        });
-      },
-      error: (err) => {
-        console.log('You got an error!')
-      }
-
+  changeView(newView) {
+    this.setState({
+      view: newView
     });
   }
 
-  // need to add functionality for redirecting to signup page when signup button is clicked
-  render () {
-    if(this.state.calendarRedirect) {
-      return <Redirect to='/calendar' />;
+  renderView() {
+    const {view} = this.state;
+
+    if (view === 'signup') {
+      return <Signup changeView={this.changeView.bind(this)} />
+    } else if(view === 'calendar') {
+      return <Calendar />
+    } else {
+      return <Signin changeView={this.changeView.bind(this)} />
     }
+  }
 
+  render() {
     return (
-      <Form horizontal>
-        <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={2}>
-            Username
-          </Col>
-          <Col sm={10}>
-            <FormControl
-            type="username"
-            value={this.state.username}
-            onChange={this.handleUsernameChange.bind(this)}
-            placeholder="Username" />
-          </Col>
-        </FormGroup>
-
-        <FormGroup controlId="formHorizontalPassword">
-          <Col componentClass={ControlLabel} sm={2}>
-            Password
-          </Col>
-          <Col sm={10}>
-            <FormControl
-            type="password"
-            value={this.state.password}
-            onChange={this.handlePasswordChange.bind(this)}
-            placeholder="Password" />
-          </Col>
-        </FormGroup>
-
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Checkbox>Remember me</Checkbox>
-          </Col>
-        </FormGroup>
-
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Button type="button" onClick={this.onButtonClick.bind(this)}>Sign in</Button>
-          </Col>
-        </FormGroup>
-
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Link to='/signup'>Sign up</Link>
-          </Col>
-        </FormGroup>
-      </Form>
-    )
+      <div>
+        <div className="main">
+          {this.renderView()}
+        </div>
+      </div>
+    );
   }
 }
 
-ReactDOM.render((
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path='/' component={App} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/calendar' component={Calendar} />
-      </Switch>
-    </div>
-  </Router>
-), document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
